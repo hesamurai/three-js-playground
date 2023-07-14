d14dd8a453e9f44ba0927b39a3bc021e7bc55a38
Hesam Bahrami <hesam.bahrami@autodesk.com>
2023-07-14 15:11:38 +0100

    refactor imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style.css'
 
-import * as THREE from 'three';
+import {
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
