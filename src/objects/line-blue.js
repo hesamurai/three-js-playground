bd3ce814fd5f2a1dd5da9d9fe5221cb1796219ff
Hesam Bahrami <hesam.bahrami@autodesk.com>
2023-07-14 16:11:24 +0100

    minor refactor

--- src/objects/line-blue.js
+++ src/objects/line-blue.js
@@ -1,14 +1,13 @@
 import { BufferGeometry, Line, LineBasicMaterial, Vector3 } from 'three';
 
-const material = new LineBasicMaterial( { color: 0x0000ff } );
-
-const points = [];
-points.push(new Vector3(-3, 0, 0));
-points.push(new Vector3(0, 3, 0));
-points.push(new Vector3(3, 0, 0));
+const points = [
+  new Vector3(-3, 0, 0),
+  new Vector3(0, 3, 0),
+  new Vector3(3, 0, 0),
+];
 
 const geometry = new BufferGeometry().setFromPoints(points);
-
+const material = new LineBasicMaterial( { color: 0x0000ff } );
 const line = new Line(geometry, material);
 
 export default line;
