1009e48f1b7e5eb517e3409d34132f10b6e899bd
Hesam Bahrami <hesam.bahrami@autodesk.com>
2023-08-25 16:31:27 +0100

    show the Japanese wave painting on the box

--- src/objects/box-green.js
+++ src/objects/box-green.js
@@ -1,7 +1,10 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshBasicMaterial, TextureLoader } from 'three';
+import wave from '../images/wave.jpg';
 
 const geometry = new BoxGeometry( 1, 1, 1 );
-const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new MeshBasicMaterial({
+  map: new TextureLoader().load(wave),
+});
 const cube = new Mesh( geometry, material );
 
 export default cube;
